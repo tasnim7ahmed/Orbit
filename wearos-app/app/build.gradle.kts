686b3aba5969e180327a129247ca6d32b75b5f71
tasnim7ahmed <tasnimahmed@iut-dhaka.edu>
2026-09-20 20:13:36 -0700

    Version 1.1.0

--- wearos-app/app/build.gradle.kts
+++ wearos-app/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.wearos.ancsbridge"
         minSdk = 33 // Wear OS 4
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     signingConfigs {
